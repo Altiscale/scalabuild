6698c57905aa6a9bdcc7df36c2d47d25fd04ff36
alee-altiscale <alee@altiscale.com>
2014-05-30 18:00:05 -0700

    fix mock cfg path and clean up parameters

--- scripts/build.sh
+++ scripts/build.sh
@@ -104,13 +104,13 @@ mock -vvv --configdir=$curr_dir -r altiscale-scala-centos-6-x86_64.runtime --res
 
 if [ $? -ne "0" ] ; then
   echo "fail - mock RPM build failed"
-  mock --clean
-  mock --scrub=all
+  mock --configdir=$curr_dir -r altiscale-scala-centos-6-x86_64.runtime --clean
+  mock --configdir=$curr_dir -r altiscale-scala-centos-6-x86_64.runtime --scrub=all
   exit -99
 fi
 
-mock --clean
-mock --scrub=all
+mock --configdir=$curr_dir -r altiscale-scala-centos-6-x86_64.runtime --clean
+mock --configdir=$curr_dir -r altiscale-scala-centos-6-x86_64.runtime --scrub=all
 
 echo "ok - build Completed successfully!"
 
